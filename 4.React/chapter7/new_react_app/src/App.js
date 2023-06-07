98d90d9d0022366298d2a0e81dac46cc398c39b2
beedrey07 <beedrey07@gmail.com>
2023-06-07 13:19:05 +0200

    first commit

---
+++ 4.React/chapter7/new_react_app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import GitHub from './GitHub';
+
+class App extends Component {
+  render() {
+    const isValid = true;
+    return (
+      <div>
+        <GitHub />
+      </div>
+    );
+  }
+}
+export default App;
