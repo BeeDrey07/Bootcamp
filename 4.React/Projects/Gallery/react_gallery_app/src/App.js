cd0c6287ab55c79a6c8a5d9b4a0f01a57d954a54
beedrey07 <beedrey07@gmail.com>
2023-06-14 09:17:22 +0200

    fifth_commit

---
+++ 4.React/Projects/Gallery/react_gallery_app/src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+function App() {
+  return (
+    <div>
+      <h1>Meet our employees!</h1>
+      <Gallery />
+    </div>
+  );
+}
+export default App;
---
+++ 4.React/Projects/Gallery/react_gallery_app/src/portfolio-gallery/Data.js
@@ -0,0 +1,117 @@
+const data = [
+  {
+    id: 1,
+    image: "./images/generalManager.png",
+    title: "General Manager",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    category:"Management"
+  },
+  {
+    id: 2,
+    image: "./images/financialManager.png",
+    title: "Financial Manager",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    category: "Finance"
+  },
+    {
+      id: 3,
+      image: "./images/accountant.png",
+      title: "Accountant",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Finance"
+    },
+    {
+      id: 4,
+      image: "./images/seniorDeveloper.png",
+      title: "Senior Developer",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"IT"
+    },
+    {
+      id: 5,
+      image: "./images/juniorDeveloper.png",
+      title: "Junior Developer",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category: "IT"
+        },
+    {
+      id: 6,
+      image: "./images/hrManager.png",
+      title: "Human Resources Manager",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Human Resources"
+    },
+    {
+      id: 7,
+      image: "./images/recruiter.png",
+      title: "Recruiter",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Human Resources"
+    },
+    {
+      id: 8,
+      image: "./images/operationsManager.png",
+      title: "Operations Manager",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Operations"
+    },
+    {
+      id: 9,
+      image: "./images/operationsAnalyst.png",
+      title: "Operations Analyst",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Operations"
+    },
+    {
+      id: 10,
+      image: "./images/marketingManager.png",
+      title: "Marketing Manager",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Marketing"
+    },
+    {
+      id: 11,
+      image: "./images/socialMediaDirector.png",
+      title: "Social Media Director",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Marketing"
+    },
+    {
+      id: 12,
+      image: "./images/salesManager.png",
+      title: "Sales Manager",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Sales"
+    },
+    {
+      id: 13,
+      image: "./images/salesSpecialist.png",
+      title: "Sales Specialist",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"Sales"
+    },
+    {
+      id: 14,
+      image: "./images/salesConsultant.png",
+      title: "Sales Consultant",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+       category : "Sales"
+    },
+]
+;
+
+export default data;
