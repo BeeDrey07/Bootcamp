a67da5b4bd75dad674cb2bdfde0e83d9a79a54df
beedrey07 <beedrey07@gmail.com>
2023-06-15 10:54:35 +0200

    seventh_commit

--- 4.React/Projects/SimpleShoppingCart/small-shopping-cart/src/components/Basket.js
+++ 4.React/Projects/SimpleShoppingCart/small-shopping-cart/src/components/Basket.js
@@ -4,7 +4,7 @@ export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
   const taxPrice = itemsPrice * 0.14;
-  const shippingPrice = itemsPrice > 1200 ? 0 : 100;
+  const shippingPrice = itemsPrice > 5000 ? 0 : 100;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
   return (
     <aside className="block col-1">
