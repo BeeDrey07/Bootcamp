a733e840a0edd685ef9e4f02eafa21a0ed6f5c0d
beedrey07 <beedrey07@gmail.com>
2023-06-15 10:29:56 +0200

    sixth_commit

---
+++ 4.React/Projects/SimpleShoppingCart/small-shopping-cart/src/data.js
@@ -0,0 +1,8 @@
+const data = {
+  products: [
+    { id: 1, name: "iPhone 11", price: 11999, image: "./images/iphone.png" },
+    { id: 2, name: "Fiat 500", price: 250000, image: "./images/fiat.png" },
+    { id: 3, name: "Instax mini", price:1889, image: "./images/instax.png" },
+  ],
+};
+export default data;
